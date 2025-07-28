d8e6577a055c09c567eaf215d84a2c0559ea4d1a
PRATHAMESHATKARE <142227723+prathameshatkare@users.noreply.github.com>
2025-07-28 15:39:42 +0530

    Update forloop.py

--- Development/Python/forloop.py
+++ Development/Python/forloop.py
@@ -6,3 +6,23 @@ for letter in stringg:
         count+=1
         
 print(f"Your word {stringg} contains {count} times letter '{lett}' ")
+
+
+
+x=int(input("enter number do you want:"))
+
+for i in range(1,int(input("how many times:"))+1):
+    print(i*x,end=" ")
+
+x=int(input("enter number do you want:"))
+sum =0
+for i in range(1,int(input("how many times:"))+1):
+    print(i*x,end=" ")
+    sum = sum+(i*x)
+    
+print()
+    
+print(f"Sum of the numbers is {sum}")
+
+
+
